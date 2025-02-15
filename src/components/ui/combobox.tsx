97d7e0a00290a8adcc397764496a4d056aeceb74
Ayu Rizkyca Awalia <ayurizkycaaa@gmail.com>
2025-02-16 00:54:49 +0700

    fix : delete unnecessary

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -19,29 +19,6 @@ import {
   PopoverTrigger,
 } from "./popover"
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-]
-
 interface Option {
   value: string;
   label: string;
